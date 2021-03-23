42fa1edd23e2a032f430850ba23daccd23ea2a32
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-23 13:25:40 +0530

    ADD: conditions for a player ladder,snake etc

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,14 +3,46 @@ import java.util.Random;
 public class SnakeAndLadder
 {
 	public static void main(String args[])
-	{
+	{	
+		// adding welcome note
 		System.out.println("Welcome to snake and ladder game!");
 		
+		// giving a start point to player
 		int playerStartPoint = 0;
+
+		// generating random number for dice to roll 1-6
 		Random rand = new Random();
 		int diceRoll = (rand.nextInt(6))+1;
+
+		// generating random numbers for play 0-2
+		Random ran = new Random();
+		int playCheck = ran.nextInt(3);
+	
+		int playerPosition = 0;
 		
-		System.out.println(diceRoll);
-	}
+		// writing a swtich case for play check  
+		switch (playCheck) {
+			case 0:
+				playerPosition = playerPosition;
+				System.out.println("Sorry no luck! You can't play.");
+				break;
+
+			case 1:
+				playerPosition = diceRoll;
+				System.out.println("Hurray you got ladder! You are moved by " + diceRoll + " positions.");
+				break;
+			
+			case 2:
+				playerPosition = playerPosition - diceRoll;
+				System.out.println("Hissss ! You are bitten by snake");
+				break;
+			
+			default:
+				System.out.println("Invalid Option");
+
+			}
+			System.out.println("Player position: " + playerPosition);
+	}	
+
 }
 		
