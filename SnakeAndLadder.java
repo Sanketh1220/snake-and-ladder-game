3f56b31de6508f61e2cb15dc27e627673b9ac3b8
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-23 15:09:58 +0530

    ADD: 2 player game, printing a result, Ladder advantage.

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -8,62 +8,121 @@ public class SnakeAndLadder
 		System.out.println("Welcome to snake and ladder game!");
 		
 		// giving a start point to player
-		int playerStartPoint = 0;
+		int player1StartPoint = 0, player2StartPoint = 0;
 
-		int playerPosition = 0, playerPreviousPoint = 0, diceCount = 0;
-		
+		int player1Position = 0, player2Position = 0, player1PreviousPoint = 0, player2PreviousPoint = 0, dice1Count = 0, dice2Count = 0;
+
 		// while loop for fetching result
-		while(playerPosition <= 100){
+		while ((player1Position <= 100) && (player2Position <= 100)) {
 
-			// generating random number for dice to roll 1-6
-      	Random rand = new Random();
-      	int diceRoll = (rand.nextInt(6))+1;
-			diceCount = diceCount + 1;
+			// generating random number for dice to roll 1-6 for player 1
+      	Random rand1 = new Random();
+      	int dice1Roll = (rand1.nextInt(6))+1;
+			dice1Count = dice1Count + 1;
 
-      	// generating random numbers for play 0-2
-      	Random ran = new Random();
-      	int playCheck = ran.nextInt(3);
+			// generating random number for dice to roll 1-6 for player 2
+         Random rand2 = new Random();
+         int dice2Roll = (rand2.nextInt(6))+1;
+         dice2Count = dice2Count + 1;
 
-			playerPreviousPoint = playerPosition;
+      	// generating random numbers for play 0-2 for player 1
+      	Random ran1 = new Random();
+      	int playCheck1 = ran1.nextInt(3);
 
+			// generating random numbers for play 0-2 for player 2
+         Random ran2 = new Random();
+         int playCheck2 = ran2.nextInt(3);
+	
+			player1PreviousPoint = player1Position;
+			player2PreviousPoint = player2Position;
+			
 			// writing a swtich case for play check  
-			switch (playCheck) {
+			switch (playCheck1) {
 				case 0:
-					playerPosition = playerPosition;
-					//System.out.println("Sorry no luck! You can't play.");
+					player1Position = player1Position;
 					break;
 
 				case 1:
-					playerPosition = diceRoll + playerPosition;
-					//System.out.println("Hurray you got ladder! You are moved by " + diceRoll + " positions.");
+					player1Position = dice1Roll + player1Position;
+					
+					Random random1 = new Random();
+         		int extraDice1 = (random1.nextInt(6))+1;
+         		dice1Count = dice1Count + 1;
+				
+					player1Position = player1Position + extraDice1; 				
+
 					break;
 			
 				case 2:
-					playerPosition = playerPosition - diceRoll;
-					//System.out.println("Hissss ! You are bitten by snake");
+					player1Position = player1Position - dice1Roll;
 					break;
 			
 				default:
 					System.out.println("Invalid Option");
 
 			}
-			// player position each time dice is rolled
-			System.out.println("Player position after dice roll " + playerPosition);
+
+			// writing a swtich case for play check for player 2
+         switch (playCheck2) {
+            case 0:
+               player2Position = player2Position;
+               break;
+
+            case 1:
+               player2Position = dice2Roll + player2Position;
+      			
+					Random random2 = new Random();
+               int extraDice2 = (random2.nextInt(6))+1;
+               dice2Count = dice2Count + 1;
+
+               player2Position = player2Position + extraDice2;         
+		
+					break;
+
+            case 2:
+               player2Position = player2Position - dice2Roll;
+               break;
+
+            default:
+               System.out.println("Invalid Option");
+
+         }
+
+			// player1 position each time dice1 is rolled
+			//System.out.println("Player1 position after dice1 roll " + player1Position);
+
+			// player2 position each time dice1 is rolled
+         //System.out.println("Player2 position after dice2 roll " + player2Position);
 								
-			if (playerPosition < 0){
-				playerPosition = playerStartPoint;
-			}else if (playerPosition > 100){
-				playerPosition = playerPosition - playerPreviousPoint;
-			}else if (playerPosition == 100){
+			if (player1Position < 0){
+				player1Position = player1StartPoint;
+			}else if (player1Position > 100){
+				player1Position = player1Position - player1PreviousPoint;
+			}else if (player1Position == 100){
 				break;
 			}
+
+			if (player2Position < 0){
+            player2Position = player2StartPoint;
+         }else if (player2Position > 100){
+            player2Position = player2Position - player2PreviousPoint;
+         }else if (player2Position == 100){
+            break;
+         }
+
 		}
-		// printing dice count
-		System.out.println("No.of Times dice has been rolled: " + diceCount);
-
+		// printing dice1 count
+		//System.out.println("No.of Times dice1 has been rolled for player1: " + dice1Count);
+		
+		// printing dice2 count
+      //System.out.println("No.of Times dice2 has been rolled for player2: " + dice2Count);
+		
 		// end result
-		System.out.println("Player wins!, Game Over.");
-	}	
-
+		if(player1Position == 100){
+		System.out.println("Player 1 wins!, Game Over.");
+		} else if (player2Position == 100){
+		System.out.println("Player 2 wins!, Gamer Over.");
+		}
+	}
 }
 		
