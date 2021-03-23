1ad576f2249e294771eb34aaabf68cdfabf339dd
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-23 12:54:53 +0530

    ADD: start point of player

---
+++ SnakeAndLadder.java
@@ -0,0 +1,11 @@
+public class SnakeAndLadder
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome to snake and ladder game!");
+		
+		int playerStartPoint = 0;
+	
+	}
+}
+		
