d536ade1a0d701d35185e9d12b1917fa28763bc9
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-23 13:47:02 +0530

    ADD: while loop included to make position of player reach 100

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -10,38 +10,45 @@ public class SnakeAndLadder
 		// giving a start point to player
 		int playerStartPoint = 0;
 
-		// generating random number for dice to roll 1-6
-		Random rand = new Random();
-		int diceRoll = (rand.nextInt(6))+1;
-
-		// generating random numbers for play 0-2
-		Random ran = new Random();
-		int playCheck = ran.nextInt(3);
-	
 		int playerPosition = 0;
-		
-		// writing a swtich case for play check  
-		switch (playCheck) {
-			case 0:
-				playerPosition = playerPosition;
-				System.out.println("Sorry no luck! You can't play.");
-				break;
 
-			case 1:
-				playerPosition = diceRoll;
-				System.out.println("Hurray you got ladder! You are moved by " + diceRoll + " positions.");
-				break;
+		while(playerPosition <= 100){
+
+			// generating random number for dice to roll 1-6
+      	Random rand = new Random();
+      	int diceRoll = (rand.nextInt(6))+1;
+
+      	// generating random numbers for play 0-2
+      	Random ran = new Random();
+      	int playCheck = ran.nextInt(3);
+
+			// writing a swtich case for play check  
+			switch (playCheck) {
+				case 0:
+					playerPosition = playerPosition;
+					//System.out.println("Sorry no luck! You can't play.");
+					break;
+
+				case 1:
+					playerPosition = diceRoll + playerPosition;
+					//System.out.println("Hurray you got ladder! You are moved by " + diceRoll + " positions.");
+					break;
 			
-			case 2:
-				playerPosition = playerPosition - diceRoll;
-				System.out.println("Hissss ! You are bitten by snake");
-				break;
+				case 2:
+					playerPosition = playerPosition - diceRoll;
+					//System.out.println("Hissss ! You are bitten by snake");
+					break;
 			
-			default:
-				System.out.println("Invalid Option");
+				default:
+					System.out.println("Invalid Option");
 
 			}
-			System.out.println("Player position: " + playerPosition);
+			
+			if(playerPosition < 0){
+				playerPosition = playerStartPoint;
+			}
+		}
+		System.out.println("Player wins!, Game Over.");
 	}	
 
 }
