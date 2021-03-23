f7e0005054942a03f9c43e5630d1d961d8c0abd6
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-23 13:00:52 +0530

    ADD: generated a dice roll from 1-6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class SnakeAndLadder
 {
 	public static void main(String args[])
@@ -5,7 +7,10 @@ public class SnakeAndLadder
 		System.out.println("Welcome to snake and ladder game!");
 		
 		int playerStartPoint = 0;
-	
+		Random rand = new Random();
+		int diceRoll = (rand.nextInt(6))+1;
+		
+		System.out.println(diceRoll);
 	}
 }
 		
